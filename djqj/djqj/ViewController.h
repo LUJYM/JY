96a5913a8a253dd191eb449c1c8e9d3ec3388cd9
apple <tanzhanwu@Neo.local>
2016-12-01 10:46:51 +0800

    创建了一个

---
+++ djqj/djqj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  djqj
+//
+//  Created by 谭占武 on 2016/12/1.
+//  Copyright © 2016年 谭占武. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ djqj/djqj/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  djqj
+//
+//  Created by 谭占武 on 2016/12/1.
+//  Copyright © 2016年 谭占武. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+
+
+@end
