0f37e7ee10c14a10c252764aaa7c990a7a48168c
apple <tanzhanwu@Neo.local>
2016-12-01 10:42:28 +0800

    创建了一个项目

---
+++ jiajsia/jiajsia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jiajsia
+//
+//  Created by 谭占武 on 2016/12/1.
+//  Copyright © 2016年 谭占武. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jiajsia/jiajsia/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  jiajsia
+//
+//  Created by 谭占武 on 2016/12/1.
+//  Copyright © 2016年 谭占武. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+
+@end
